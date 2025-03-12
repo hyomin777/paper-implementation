442932cb599db667694d61198c971f51cc0f1ca5
hyomin777 <hyomin2677@gmail.com>
2025-03-12 23:50:51 +0900

    inception

--- GoogLeNet/basic_layer.py
+++ GoogLeNet/basic_layer.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 
 class BasicConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels)
-        self.relu = nn.ReLU()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.relu(self.conv(x))
@@ -17,7 +17,7 @@ class BasicFC(nn.Module):
     def __init__(self, in_features, out_features):
         super().__init__()
         self.fc = nn.Linear(in_features, out_features)
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.relu(self.fc(x))
--- GoogLeNet/inception.py
+++ GoogLeNet/inception.py
@@ -1,3 +1,41 @@
 import torch
 import torch.nn as nn
+from basic_layer import BasicConv2d, BasicFC
 
+
+class Inception(nn.Module):
+    def __init__(
+            self, 
+            in_channels, 
+            conv_1x1,
+            conv_3x3,
+            conv_5x5,
+            conv_pool
+            ):
+        super().__init__()
+
+        self.branch1 = BasicConv2d(in_channels, conv_1x1, kernel_size=1)
+        self.branch2 = nn.Sequential(
+            BasicConv2d(in_channels, conv_1x1, kernel_size=1),
+            BasicConv2d(conv_1x1, conv_3x3, kernel_size=3, padding=1)
+        )
+        self.branch3 = nn.Sequential(
+            BasicConv2d(in_channels, conv_1x1, kernel_size=1),
+            BasicConv2d(conv_1x1, conv_5x5, kernel_size=5, padding=2)
+        )
+        self.branch4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            BasicConv2d(in_channels, conv_pool, kernel_size=1)
+        )
+
+
+    def forward(self, x):
+        branch1 = self.branch1(x)
+        branch2 = self.branch2(x)
+        branch3 = self.branch3(x)
+        branch4 = self.branch4(x)
+        out = [branch1, branch2, branch3, branch4]
+
+        out = torch.cat(out, 1)
+
+        return out
