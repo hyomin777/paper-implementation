0d8b61de4da21abecf31fdfc2496d4e3bc8ce93f
hyomin777 <hyomin2677@gmail.com>
2025-03-14 13:14:18 +0900

    config

---
+++ GoogLeNet/config.py
@@ -0,0 +1,22 @@
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR100
+from torch.utils.data import DataLoader
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+batch_size = 64
+lr = 0.01
+epochs = 100
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+train_set = CIFAR100(root='./data', train=True, download=True, transform=transform)
+test_set = CIFAR100(root='./data', train=False, download=True, transform=transform)
+
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
--- GoogLeNet/train.py
+++ GoogLeNet/train.py
@@ -1,35 +1,18 @@
 import torch
 import torch.optim as optim
 from torch import Tensor
-from torch.utils.data import DataLoader
-
-import torchvision.transforms as transforms
-from torchvision.datasets import CIFAR100
 
 from tqdm import tqdm
-
 from model import GoogLeNet
 from loss import GoogLeNetLoss
+from config import device, lr, epochs, train_loader
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu' 
-batch_size = 64
-lr = 0.01
-epochs = 100
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-])
-
-train_set = CIFAR100(root='./data', train=True, download=True, transform=transform)
-train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-
-test_set = CIFAR100(root='./data', train=False, download=True, transform=transform)
-test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 def train_main():
     model = GoogLeNet(num_classes=100).to(device)
     model.load_state_dict(torch.load("model_weights.pth"))
+    model.train()
+
     criterion = GoogLeNetLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
